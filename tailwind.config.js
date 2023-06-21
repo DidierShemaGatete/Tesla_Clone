48c0eb1b2565ec1f11138b4499116d69327b4614
DidierShemaGatete <didiershemagate01@gmail.com>
2023-06-21 23:40:57 +0300

    mobile view init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      borderColor:{
+        black: '#000000'
+      }
+    
+    },
+  },
+  plugins: [],
+}
+
